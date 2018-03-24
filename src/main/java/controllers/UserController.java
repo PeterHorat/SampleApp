59d0c2cc813ef643fc56b912e52ffb421e8b0b95
PeterHorat <peterhorat@hispeed.ch>
2018-03-24 14:13:59 +0100

    init

--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import models.Track;
 import models.User;
 import services.UserService;
 
@@ -7,9 +8,13 @@ import javax.inject.Inject;
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
 import javax.ws.rs.GET;
+import javax.ws.rs.OPTIONS;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/users")
 public class UserController {
@@ -19,12 +24,32 @@ public class UserController {
     @Path("/all")
     @GET
     @Produces("application/json")
-    public JsonArray getAll(){
+    public JsonArray getAll() {
         JsonArrayBuilder builder = Json.createArrayBuilder();
-        for (User user : userService.getAll()){
+        for (User user : userService.getAll()) {
             builder.add(Json.createObjectBuilder().add("email", user.getEmail()));
         }
         return builder.build();
+    }
+
+    @Path("/user")
+    @GET
+    public Response getUser() {
+        String json = "{etwas: jaja}";
+        return Response.ok(json, MediaType.APPLICATION_JSON).build();
+    }
+
+    @GET
+    @Path("/track")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Track getTrackInJSON() {
+
+        Track track = new Track();
+        track.setTitle("Enter Sandman");
+        track.setSinger("Metallica");
+
+        return track;
+
     }
 
 }
